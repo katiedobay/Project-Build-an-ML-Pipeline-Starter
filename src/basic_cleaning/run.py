ed595d018eb8aba461176f575d5ee4170d104705
katiedobay <kaitlindobay@gmail.com>
2026-09-04 17:29:26 +0000

    finished basic cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -54,48 +54,48 @@ def go(args):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="A very basic data cleaning")
-  
-    parser.add_argument(
-        "--input_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
-    )
 
     parser.add_argument(
-        "--output_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
-    )
+        "--input_artifact",
+        type=str,
+        help="Input artifact to be cleaned",
+        required=True
+)
 
     parser.add_argument(
-        "--output_type", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
-    )
+        "--output_artifact",
+        type=str,
+        help="Output artifact for cleaned data",
+        required=True
+)
 
     parser.add_argument(
-        "--output_description", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
-    )
+        "--output_type",
+        type=str,
+        help="Type of the output dataset",
+        required=True
+)
 
     parser.add_argument(
-        "--min_price", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
-    )
+        "--output_description",
+        type=str,
+        help="Description of the output dataset",
+        required=True
+)
+
+    parser.add_argument(
+        "--min_price",
+        type=float,
+        help="Minimum house price to be considered",
+        required=True
+)
 
     parser.add_argument(
         "--max_price",
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
-    )
+        type=float,
+        help="Maximum house price to be considered",
+        required=True
+)
 
 
     args = parser.parse_args()
